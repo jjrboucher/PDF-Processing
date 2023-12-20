e5e06c93bbbf3c269d6045a0fdd61444beca6ce4
jjrboucher <jjrboucher@users.noreply.github.com>
2023-12-20 10:41:23 -0600

    Minor tweak to the file
    Using a variable for output file rather than a constant, and added quotes around *.pdf in the find command.

--- pdf-triage.sh
+++ pdf-triage.sh
@@ -7,24 +7,30 @@
 #
 # You can modify the script to extract other fields if they are of interest to you.
 
-if test -f pdf-triage.csv; then
-	echo "pdf-triage.csv already exists. Rename or move and re-run the script."
+output_file="pdf-triage.csv"
+
+if test -f $output_file; then
+	echo "$output_file already exists. Rename or move and re-run the script."
 	exit
 fi
 
 OIFS="$IFS" # Original Field Separator
 IFS=$'\n' # New field separator = new line
 
-a=$(find . -iname *.pdf) # Find all PDFs recursively from current folder
-echo "file", "# of images", "Author", "# of versions" >pdf-triage.csv # write headers to csv
+a=$(find . -iname "*.pdf") # Find all PDFs recursively from current folder
+echo "file", "CreateDate","Modify Date", "# of images", "Author", "# of versions", "hash" >$output_file # write headers to csv
 for i in $a # loop through each item
 do
 	image_count=$(pdfimages -list $i | wc -l) # get only # of lines in output (# of images)
 	image_count=$((image_count-2)) # substract 2 lines - headers - to get actual # of images
 	author=$(exiftool -S -s -author $i) # get author of the document without the tag name
+	create_date=$(exiftool -S -s -CreateDate $i)
+	modify_date=$(exiftool -S -s -ModifyDate $i)
+	hash=$(md5sum $i)
 	offsets=($(grep --only-matching --byte-offset --text "%%EOF" "$i"| cut -d : -f 1)) # find all %%EOF instances in the PDF
 	version_count=${#offsets[@]} # Number of instances of %%EOF
-	echo $i, $image_count, $author, $version_count >>pdf-triage.csv # append results to csv file in current directory
+	echo $i, $create_date, $modify_date, $image_count, $author, $version_count, $hash >>$output_file # append results to csv file in current directory
 done
 
+echo "Results can be found in $output_file."
 IFS="$OIFS" # restore IFS to original
